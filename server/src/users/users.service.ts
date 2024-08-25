6e462ef648479552c199424d05bcae580fbd807d
Marc Edison <suanieeee@yahoo.com>
2024-08-26 02:01:52 +0800

    ```text chore: Update UsersService to handle user authentication with JWT
    The code changes in users.service.ts update the UsersService to handle user authentication using JWT (JSON Web Tokens). The InsertUser method now hashes the password before storing it in the database, and a new ReadLoginUser method is added to verify the email and password and generate a JWT token for successful logins. This change enhances the functionality of the users module by adding secure authentication capabilities.

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,14 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IUsers } from './users.interface';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel('User') private readonly UserModel: Model<IUsers>) { }
+    constructor(
+        @InjectModel('User')
+        private readonly UserModel: Model<IUsers>,
+        private readonly jwtService: JwtService
+    ) { }
 
     async InsertUser({ email, password }: { email: string, password: string }) {
-        await this.UserModel.create({ email, password })
+        const salt = await bcrypt.genSalt()
+        const hashedpassword = await bcrypt.hash(password, salt)
+
+        return await this.UserModel.create({ email, password: hashedpassword })
     }
 
+    async ReadLoginUser({ email, password }: { email: string, password: string })
+        : Promise<{ success: boolean, message: string, access_token: string }> {
+        const isemail = await this.UserModel.findOne({ email })
+        if (!isemail) throw new UnauthorizedException('Email does not recognized.')
+
+        const ispassword = await bcrypt.compare(password, isemail.password)
+        if (!ispassword) throw new UnauthorizedException('Password is incorrect.')
+
+        const payload = { sub: isemail._id }
+        const jwt = await this.jwtService.signAsync(payload)
+        return { success: true, message: 'Logged in successfully!', access_token: jwt }
+    }
 }
