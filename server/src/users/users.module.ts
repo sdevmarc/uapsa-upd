a6051b40cdbf8a0957b39de95de43d55c69c46fd
Marc Edison <suanieeee@yahoo.com>
2024-08-26 02:01:43 +0800

    chore: Update UsersModule imports
    The code changes in users.module.ts update the imports for the UsersModule. The ConfigModule and JwtModule are now imported and configured to handle JWT token generation and verification. This change enhances the functionality of the UsersModule by adding support for JWT authentication.

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -3,10 +3,28 @@ import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './users.schema';
 import { UsersController } from './users.controller';
-
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './users.constants';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRET_JWT_TOKEN'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   providers: [UsersService],
   controllers: [UsersController]
 })
-export class UsersModule {}
+export class UsersModule { }
