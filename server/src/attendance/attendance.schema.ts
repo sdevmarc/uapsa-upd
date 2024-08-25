c2311104f5c63841acfd8b9926ec271fbac0930f
Marc Edison <suanieeee@yahoo.com>
2024-08-26 02:01:19 +0800

    feat: Add QR reference to AttendanceSchema
    The code changes in attendance.schema.ts add a new field "qr" to the AttendanceSchema. This field is a reference to the QR model and is required. This change enhances the attendance functionality by associating each attendance record with a specific QR code. The commit message follows the established convention of using the "feat" type for new feature additions.

--- server/src/attendance/attendance.schema.ts
+++ server/src/attendance/attendance.schema.ts
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 export const AttendanceSchema = new mongoose.Schema({
+    qr: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Qr',
+        required: true
+    },
     date: {
         required: true,
         type: Date,
