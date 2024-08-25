bd8b53b766b12f5beeac827522dca7ce85aa285b
Marc Edison <suanieeee@yahoo.com>
2024-08-26 02:01:39 +0800

    chore: Add createUser and loginUser endpoints to UsersController
    The code changes in users.controller.ts add two new endpoints, `createUser` and `loginUser`, to the UsersController. These endpoints handle the creation and login of users by accepting the email and password as request body parameters. This change enhances the functionality of the users module by providing the necessary API endpoints for user management.

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService: UsersService) { }
+
+    @Post('create-user')
+    async CreateUser(@Body() { email, password }: { email: string, password: string }) {
+        try {
+            return await this.userService.InsertUser({ email, password })
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'User not created successfully!' }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @Post('login-user')
+    async LoginUser(@Body() { email, password }: { email: string, password: string }) {
+        try {
+            return this.userService.ReadLoginUser({ email, password })
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'User failed to login!' }, HttpStatus.BAD_REQUEST)
+        }
+    }
+}
