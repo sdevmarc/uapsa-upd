e602c12d9d5ffe76c8305c1ea6640ed24886bdfc
sdevmarc <suanieeee@yahoo.com>
2024-08-26 17:03:10 +0800

    refactor: Add InsertAttended, InsertAbsent, and ResetAttendance methods to AttendanceService
    The code changes in attendance.service.ts add three new methods to the AttendanceService: InsertAttended, InsertAbsent, and ResetAttendance. These methods handle the creation of attendance records for attended and absent events, as well as resetting the attendance data. This change enhances the functionality of the attendance module by providing the necessary methods for managing attendance records. The commit message follows the established convention of using the "refactor" type for code changes that improve code structure or organization.

--- server/src/attendance/attendance.service.ts
+++ server/src/attendance/attendance.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IAttendance } from './attendance.interface';
+import { IPoints } from 'src/points/points.interface';
 
 @Injectable()
-export class AttendanceService {}
+export class AttendanceService {
+    constructor(
+        @InjectModel('Attendance') private readonly AttendanceModel: Model<IAttendance>
+    ) { }
+
+    async InsertAttended({ qr }: IAttendance)
+        : Promise<{ success: boolean, message: string }> {
+
+        await this.AttendanceModel.findOneAndUpdate(
+            { qr },
+            { $inc: { attended: 1 } },
+            { new: true, upsert: true }
+        )
+        
+        return { success: true, message: 'Attended created sucessfully!' }
+    }
+
+    async InsertAbsent({ qr }: IAttendance)
+        : Promise<{ success: boolean, message: string }> {
+        await this.AttendanceModel.findOneAndUpdate(
+            { qr },
+            { $inc: { absences: 1 } },
+            { new: true, upsert: true }
+        )
+
+        return { success: true, message: 'Absent created sucessfully!' }
+    }
+
+    async ResetAttendance()
+        : Promise<{ success: boolean, message: string }> {
+        await this.AttendanceModel.deleteMany({})
+
+        return { success: true, message: 'Attendance resetted sucessfully!' }
+    }
+}
