9a3007281839a1a2632c4e3387e103ac07882645
Marc Edison <suanieeee@yahoo.com>
2024-08-26 23:47:58 +0800

    feat: Add Attendance and Points schemas and services to QrModule
    The code changes in qr.module.ts add the Attendance and Points schemas and services to the QrModule. This change enhances the functionality of the module by allowing it to work with attendance and points data. The commit message follows the established convention of using the "feat" type for new feature additions.

--- server/src/qr/qr.module.ts
+++ server/src/qr/qr.module.ts
@@ -3,9 +3,17 @@ import { QrService } from './qr.service';
 import { QrController } from './qr.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QRSchema } from './qr.schema';
+import { AttendanceSchema } from 'src/attendance/attendance.schema';
+import { PointsSchema } from 'src/points/points.schema';
+import { AttendanceService } from 'src/attendance/attendance.service';
+import { PointsService } from 'src/points/points.service';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'Qr', schema: QRSchema }])],
+    imports: [MongooseModule.forFeature([
+        { name: 'Qr', schema: QRSchema },
+        { name: 'Attendance', schema: AttendanceSchema },
+        { name: 'Point', schema: PointsSchema }
+    ])],
     providers: [QrService],
     controllers: [QrController]
 })
