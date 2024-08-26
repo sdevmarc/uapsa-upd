a870af39e1abb00b239d8ae3f5718b3f9a1b5dc5
sdevmarc <suanieeee@yahoo.com>
2024-08-26 19:28:06 +0800

    feat: Add view all and view one user endpoints to UsersController

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,10 +1,33 @@
-import { Body, Controller, HttpException, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly userService: UsersService) { }
+
+    @UseGuards(AuthGuard)
+    @Get()
+    async ViewAllUsers() {
+        try {
+            return await this.userService.findAll()
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Failed to retrieved all users.' }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @UseGuards(AuthGuard)
+    @Get(':email')
+    async ViewOneIUser(
+        @Param() { email }: { email: string }
+    ) {
+        try {
+            return await this.userService.findOne({ email })
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'User data failed to retrieved!' }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
 
     @Post('create-user')
     async CreateUser(@Body() { qr, email, password }: { qr: string, email: string, password: string }) {
