0647dc2a35875e5d81dabfd7682ec544923e2a01
sdevmarc <suanieeee@yahoo.com>
2024-08-26 17:10:09 +0800

    refactor: Add InsertQr method to QrService
    The code changes in qr.service.ts add a new method, InsertQr, to the QrService. This method handles the insertion of QR codes by accepting the idNumber, name, and degree as parameters and creating a new QR record in the database. This change enhances the functionality of the QR module by providing the necessary method for creating QR codes. The commit message follows the established convention of using the "refactor" type for code changes that improve code structure or organization.

--- server/src/qr/qr.service.ts
+++ server/src/qr/qr.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IQr } from './qr.interface';
 
 @Injectable()
-export class QrService {}
+export class QrService {
+    constructor(
+        @InjectModel('Qr')
+        private readonly QrModel: Model<IQr>
+    ) { }
+
+    async InsertQr({ idNumber, name, degree }: IQr)
+        : Promise<{ success: boolean, message: string }> {
+        await this.QrModel.create({ idNumber, name, degree })
+        return { success: true, message: 'Qr created successfully!' }
+    }
+}
