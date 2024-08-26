c80233e7df2cb1698d7aec3fdce679aa395825f6
sdevmarc <suanieeee@yahoo.com>
2024-08-26 17:10:19 +0800

    refactor: Update UsersController to include QR code in CreateUser and ReadLoginUser methods
    The code changes in users.controller.ts modify the CreateUser and ReadLoginUser methods to include the QR code parameter. This change enhances the functionality of the Users module by allowing the creation and login of users with a QR code. The commit message follows the established convention of using the "refactor" type for code changes that improve code structure or organization.

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -6,11 +6,11 @@ export class UsersController {
     constructor(private readonly userService: UsersService) { }
 
     @Post('create-user')
-    async CreateUser(@Body() { email, password }: { email: string, password: string }) {
+    async CreateUser(@Body() { qr, email, password }: { qr: string, email: string, password: string }) {
         try {
-            return await this.userService.InsertUser({ email, password })
+            return await this.userService.InsertUser({ qr, email, password })
         } catch (error) {
-            throw new HttpException({ success: false, message: 'User not created successfully!' }, HttpStatus.BAD_REQUEST)
+            throw new HttpException({ success: false, message: 'User not created successfully!', error }, HttpStatus.BAD_REQUEST)
         }
     }
 
@@ -19,7 +19,7 @@ export class UsersController {
         try {
             return this.userService.ReadLoginUser({ email, password })
         } catch (error) {
-            throw new HttpException({ success: false, message: 'User failed to login!' }, HttpStatus.BAD_REQUEST)
+            throw new HttpException({ success: false, message: 'User failed to login!', error }, HttpStatus.BAD_REQUEST)
         }
     }
 }
