fc712be786c9358a198135ba10c8860ae005aa97
sdevmarc <suanieeee@yahoo.com>
2024-08-26 17:03:44 +0800

    feat: Add endpoint for creating QR code
    The code changes in qr.controller.ts add a new endpoint, `create-qr`, to the QrController. This endpoint handles the creation of QR codes by accepting the idNumber, name, and degree as request body parameters. This change enhances the functionality of the QR module by providing the necessary API endpoint for creating QR codes.

--- server/src/qr/qr.controller.ts
+++ server/src/qr/qr.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
+import { QrService } from './qr.service';
+import { IQr } from './qr.interface';
 
 @Controller('qr')
-export class QrController {}
+export class QrController {
+    constructor(
+        private readonly qrService: QrService
+    ) { }
+
+    @Post('create-qr')
+    async CreateQr(@Body() { idNumber, name, degree }: IQr) {
+        try {
+            return await this.qrService.InsertQr({ idNumber, name, degree })
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Qr failed to create.' }, HttpStatus.BAD_REQUEST)
+        }
+    }
+}
