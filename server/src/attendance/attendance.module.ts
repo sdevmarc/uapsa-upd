589c928614696e4a3babea5136806c9fe43558a4
sdevmarc <suanieeee@yahoo.com>
2024-08-26 17:02:46 +0800

    refactor: Update AttendanceModule to include PointsModule
    The code changes in attendance.module.ts update the AttendanceModule to include the PointsModule. This change enhances the attendance functionality by integrating the points system into the attendance module. The commit message follows the established convention of using the "refactor" type for code changes that improve code structure or organization.

--- server/src/attendance/attendance.module.ts
+++ server/src/attendance/attendance.module.ts
@@ -3,10 +3,17 @@ import { AttendanceService } from './attendance.service';
 import { AttendanceController } from './attendance.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AttendanceSchema } from './attendance.schema';
+import { PointsSchema } from 'src/points/points.schema';
+import { PointsService } from 'src/points/points.service';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'Attendance', schema: AttendanceSchema }])],
-    providers: [AttendanceService],
+    imports: [
+        MongooseModule.forFeature([
+            { name: 'Attendance', schema: AttendanceSchema },
+            { name: 'Point', schema: PointsSchema },
+        ]),
+    ],
+    providers: [AttendanceService, PointsService],
     controllers: [AttendanceController]
 })
 export class AttendanceModule { }
