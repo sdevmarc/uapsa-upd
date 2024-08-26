0e6e46c41462e928fb7aaf51c847bc2fd5fe194d
sdevmarc <suanieeee@yahoo.com>
2024-08-26 17:03:33 +0800

    refactor: Add InsertPoint and LessPoint methods to PointsService
    The code changes in points.service.ts add two new methods, InsertPoint and LessPoint, to the PointsService. These methods handle the insertion and subtraction of points for a given QR code. This change enhances the functionality of the points module by providing the necessary methods for managing points. The commit message follows the established convention of using the "refactor" type for code changes that improve code structure or organization.

--- server/src/points/points.service.ts
+++ server/src/points/points.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IPoints } from './points.interface';
 
 @Injectable()
-export class PointsService {}
+export class PointsService {
+    constructor(
+        @InjectModel('Point')
+        private readonly PointModel: Model<IPoints>
+    ) { }
+
+    async InsertPoint({ qr }: { qr: string })
+        : Promise<{ success: boolean, message: string }> {
+        await this.PointModel.findOneAndUpdate(
+            { qr },
+            { $inc: { points: 1 } },
+            { new: true, upsert: true }
+        )
+        return { success: true, message: 'Point added to the user successfully!' }
+    }
+
+    async LessPoint({ qr }: { qr: string })
+        : Promise<{ success: boolean, message: string }> {
+        await this.PointModel.findOneAndUpdate(
+            { qr },
+            { $inc: { points: -1 } },
+            { new: true, upsert: true }
+        )
+        return { success: true, message: 'Point lessen to the user successfully!' }
+    }
+}
