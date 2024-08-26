e36459cf125e5265974f9f387cd392e02d48ab4b
sdevmarc <suanieeee@yahoo.com>
2024-08-26 17:03:20 +0800

    feat: Add endpoints for adding and subtracting points
    The code changes in points.controller.ts add two new endpoints, `add-point` and `less-point`, to the PointsController. These endpoints handle the addition and subtraction of points by accepting a QR code as a request body parameter. This change enhances the functionality of the points module by providing the necessary API endpoints for managing points. The commit message follows the established convention of using the "feat" type for new feature additions.

--- server/src/points/points.controller.ts
+++ server/src/points/points.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
+import { PointsService } from './points.service';
 
 @Controller('points')
-export class PointsController {}
+export class PointsController {
+    constructor(
+        private readonly pointService: PointsService
+    ) { }
+
+    @Post('add-point')
+    async CreatePoint(@Body() { qr }: { qr: string }) {
+        try {
+            return await this.pointService.InsertPoint({ qr })
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Point failed to add to the user.' }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @Post('less-point')
+    async MinusPoint(@Body() { qr }: { qr: string }) {
+        try {
+            return await this.pointService.LessPoint({ qr })
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Point failed to add to the user.' }, HttpStatus.BAD_REQUEST)
+        }
+    }
+}
