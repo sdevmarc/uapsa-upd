d85054aa5cbf970df9256fdcf370c53341b7638e
Marc Edison <suanieeee@yahoo.com>
2024-08-26 23:48:04 +0800

    feat: Add QR, Attendance, and Points models and services
    The code changes in qr.service.ts add the QR, Attendance, and Points models and services to the QrService. This change enhances the functionality of the module by allowing it to work with QR user data, attendance records, and points data. The commit message follows the established convention of using the "feat" type for new feature additions.

--- server/src/qr/qr.service.ts
+++ server/src/qr/qr.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IQr } from './qr.interface';
+import { IAttendance } from 'src/attendance/attendance.interface';
+import { IPoints } from 'src/points/points.interface';
 
 @Injectable()
 export class QrService {
     constructor(
-        @InjectModel('Qr')
-        private readonly QrModel: Model<IQr>
+        @InjectModel('Qr') private readonly QrModel: Model<IQr>,
+        @InjectModel('Attendance') private readonly AttendanceModel: Model<IAttendance>,
+        @InjectModel('Point') private readonly PointModel: Model<IPoints>
     ) { }
 
     async findAll()
@@ -17,11 +20,15 @@ export class QrService {
         return { success: true, message: 'Qr users retrieved successfully!', data }
     }
 
-    async findOne({ idNumber }: { idNumber: string })
-        : Promise<{ success: boolean, message: string, data: IQr }> {
-        const data = await this.QrModel.findOne({ idNumber })
-        if (!data) return { success: true, message: 'Cannot find qr user!', data }
-        return { success: true, message: 'Qr user data retrieved successfully!', data }
+    async findOne({ qr }: { qr: string })
+        : Promise<{ success: boolean, message: string, data: IQr, attendance: IAttendance, points: IPoints }> {
+
+        const data = await this.QrModel.findById(qr)
+        const attendance = await this.AttendanceModel.findOne({ qr })
+        const points = await this.PointModel.findOne({ qr })
+
+        if (!data) return { success: true, message: 'Cannot find qr user!', data, attendance, points }
+        return { success: true, message: 'Qr user data retrieved successfully!', data, attendance, points }
     }
 
     async InsertQr({ idNumber, name, degree }: IQr)
