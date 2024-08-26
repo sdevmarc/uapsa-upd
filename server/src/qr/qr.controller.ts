782e21dfb8adf04d1f4da2a44062091eefcfe3d4
sdevmarc <suanieeee@yahoo.com>
2024-08-26 19:27:56 +0800

    feat: Add view all and view one QR user endpoints
    The code changes in qr.controller.ts add two new endpoints: `ViewAllQrUsers` and `ViewOneQrUser`. These endpoints allow users to view QR user data either for all users or for a specific user identified by an ID number. This change enhances the functionality of the QRController by providing a way to retrieve QR user information. The commit message follows the established convention of using the "feat" type for new feature additions.

--- server/src/qr/qr.controller.ts
+++ server/src/qr/qr.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpException, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post, UseGuards } from '@nestjs/common';
 import { QrService } from './qr.service';
 import { IQr } from './qr.interface';
 import { AuthGuard } from 'src/auth/auth.guard';
@@ -8,6 +8,26 @@ export class QrController {
     constructor(
         private readonly qrService: QrService
     ) { }
+
+    @Get()
+    async ViewAllQrUsers() {
+        try {
+            return await this.qrService.findAll()
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Qr data failed to retrieved!' }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @Get(':idNumber')
+    async ViewOneQrUser(
+        @Param() { idNumber }: { idNumber: string }
+    ) {
+        try {
+            return await this.qrService.findOne({ idNumber })
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Qr user data failed to retrieved!' }, HttpStatus.BAD_REQUEST)
+        }
+    }
 
     @UseGuards(AuthGuard)
     @Post('create-qr')
