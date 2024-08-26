89b0ac2763bdf9a12d94f029e85ae0b96775d19b
sdevmarc <suanieeee@yahoo.com>
2024-08-26 19:28:01 +0800

    feat: Add findAll and findOne methods to QrService
    The code changes in qr.service.ts add two new methods: `findAll` and `findOne`. These methods allow retrieving QR user data either for all users or for a specific user identified by an ID number. This change enhances the functionality of the QrService by providing a way to retrieve QR user information. The commit message follows the established convention of using the "feat" type for new feature additions.

--- server/src/qr/qr.service.ts
+++ server/src/qr/qr.service.ts
@@ -10,6 +10,20 @@ export class QrService {
         private readonly QrModel: Model<IQr>
     ) { }
 
+    async findAll()
+        : Promise<{ success: boolean, message: string, data: IQr[] }> {
+        const data = await this.QrModel.find()
+        if (data.length <= 0) return { success: true, message: 'No exisisting qr users!', data }
+        return { success: true, message: 'Qr users retrieved successfully!', data }
+    }
+
+    async findOne({ idNumber }: { idNumber: string })
+        : Promise<{ success: boolean, message: string, data: IQr }> {
+        const data = await this.QrModel.findOne({ idNumber })
+        if (!data) return { success: true, message: 'Cannot find qr user!', data }
+        return { success: true, message: 'Qr user data retrieved successfully!', data }
+    }
+
     async InsertQr({ idNumber, name, degree }: IQr)
         : Promise<{ success: boolean, message: string }> {
         await this.QrModel.create({ idNumber, name, degree })
