4e3dfec80f7e2e840eb343ff0c87748cb4b7d9d3
sdevmarc <suanieeee@yahoo.com>
2024-08-26 17:02:27 +0800

    feat: Add endpoints for creating attended and absent records
    The code changes in attendance.controller.ts add two new endpoints, `create-attended` and `create-absent`, to the AttendanceController. These endpoints handle the creation of attended and absent records by accepting a QR code as a request body parameter. This change enhances the attendance functionality by providing the necessary API endpoints for recording attendance. The commit message follows the established convention of using the "feat" type for new feature additions.

--- server/src/attendance/attendance.controller.ts
+++ server/src/attendance/attendance.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
+import { AttendanceService } from './attendance.service';
+import { PointsService } from 'src/points/points.service';
 
 @Controller('attendance')
-export class AttendanceController {}
+export class AttendanceController {
+    constructor(
+        private readonly attendanceService: AttendanceService,
+        private readonly pointService: PointsService
+    ) { }
+
+    @Post('create-attended')
+    async CreateAttended(@Body() { qr }: { qr: string }) {
+        try {
+            await this.pointService.InsertPoint({ qr })
+            return await this.attendanceService.InsertAttended({ qr })
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Creating attended failed to create.', error }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @Post('create-absent')
+    async CreateAbsent(@Body() { qr }: { qr: string }) {
+        try {
+            return await this.attendanceService.InsertAbsent({ qr })
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Creating attended failed to create.', error }, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @Post('reset')
+    async DeleteAttendance() {
+        try {
+            return await this.attendanceService.ResetAttendance()
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Creating attended failed to create.', error }, HttpStatus.BAD_REQUEST)
+        }
+    }
+}
