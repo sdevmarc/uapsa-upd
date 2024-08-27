e5d52e0e79c62c7c6204f5627f281d2e9678d6d9
sdevmarc <suanieeee@yahoo.com>
2024-08-27 20:03:49 +0800

    refactor: Update QR schema and types
    Update the QR schema and types in the schema.tsx file. This change renames the schema from "expenseSchema" to "qrschemas" and updates the property names and types to match the recent changes in the data.json file. This refactor ensures consistency and improves the clarity of the code. The commit message follows the established convention of using the "refactor" type for code improvements.

--- client/src/components/data-table-components/schema.tsx
+++ client/src/components/data-table-components/schema.tsx
@@ -2,14 +2,13 @@ import { z } from "zod";
 
 // We're keeping a simple non-relational schema here.
 // IRL, you will have a schema for your data models.
-export const expenseSchema = z.object({
-    id?: z.string(),
-    label: z.string(),
-    note: z.string(),
-    category: z.string(),
-    type: z.enum(["income", "expense"]),
-    amount: z.number(),
+export const qrschemas = z.object({
+    id: z.number(),
+    name: z.string(),
+    course: z.string(),
+    attended: z.string(),
+    points: z.number(),
     date: z.string()
 });
 
-export type Expense = z.infer<typeof expenseSchema>;
+export type QR = z.infer<typeof qrschemas>;
