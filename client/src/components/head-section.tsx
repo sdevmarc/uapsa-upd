3bf1739570828592dd660f8a1c50ad8d574c18c5
sdevmarc <suanieeee@yahoo.com>
2024-08-27 21:28:20 +0800

    ```text feat: Add HeadSection component
    Add the HeadSection component to the client/src/components/head-section.tsx file. This component provides a reusable section for displaying a title and description. It includes sub-components for displaying subhead section details and a back button. The changes enhance the application by improving the structure and organization of the code.

---
+++ client/src/components/head-section.tsx
@@ -0,0 +1,39 @@
+import { IFCChildren } from "@/interface/index";
+import { useNavigate } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
+
+export default function HeadSection({ children }: IFCChildren) {
+    return (
+        <>
+            <section className="w-full py-4 flex flex-col justify-center items-start gap-2 border-b-[0.7px] border-b">
+                {children}
+            </section>
+        </>
+    )
+}
+
+export const SubHeadSectionDetails = ({ title, description }: IFCChildren) => {
+    return (
+        <div className="flex flex-col gap-2 px-8 ">
+            <h1 className="text-text font-semibold text-xl">
+                {title}
+            </h1>
+            <p className="text-text font-light text-sm">
+                {description}
+            </p>
+        </div>
+    )
+}
+
+export const BackHeadSection = () => {
+    const navigate = useNavigate()
+
+    const handleBack = () => {
+        navigate(-1)
+    }
+    return (
+        <button onClick={handleBack} className="flex items-center gap-2 h-9 px-4 rounded-lg hover:bg-black/10">
+            <IoIosArrowBack /> Back
+        </button>
+    )
+}
