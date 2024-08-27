15714cde94a40ec2a7a51292bfa9b4819091e2d8
sdevmarc <suanieeee@yahoo.com>
2024-08-27 20:04:08 +0800

    ```text feat: Update DataTable component to use QR data
    Update the DataTable component in Dashboard.tsx to use the QR data instead of the Expense data. This change includes importing the QR schema and columns, and updating the data source to use the QRData array. This feature enhances the application by displaying the QR data in the data table.

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,26 +1,21 @@
-import { columns } from "@/components/data-table-components/columns";
 import { DataTable } from "@/components/data-table-components/data-table";
-import { Expense } from "@/components/data-table-components/schema";
+import {  QR } from "@/components/data-table-components/schema";
 import Header from "@/components/header";
-import expense from '@/components/data-table-components/data.json'
+import QRData from '@/components/data-table-components/data.json'
 import { useState } from "react";
+import { qrcolumns } from "@/components/data-table-components/columns/qr-columns";
 
 export default function Dashboard() {
-    const modifiedExpense = expense.map(item => ({ id: generateId(), ...item, type: item.type as "income" | "expense" }));
-    const [data, setData] = useState<Expense[]>(modifiedExpense)
+    const [data, setData] = useState<QR[]>(QRData)
 
     return (
         <>
             <div className="w-full flex flex-col justify-center items-center">
                 <Header />
                 <div className="w-full max-w-[90rem] pt-10 px-4">
-                    <DataTable columns={columns} data={data} />
+                    <DataTable columns={qrcolumns} data={data} />
                 </div>
             </div>
         </>
     )
-}
-
-function generateId() {
-    return '1';
 }
