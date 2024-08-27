1fb78405832f16028e24b0eda8fd0ae5c7b3cd29
sdevmarc <suanieeee@yahoo.com>
2024-08-27 21:28:32 +0800

    feat: Add UserAvatar component
    Add the UserAvatar component to the client/src/components/user-avatar.tsx file. This component is responsible for rendering a user avatar with dropdown menu options for "My Account", "Profile", "Settings", "GitHub", "Support", and "Log out". The addition of this component enhances the application by providing a user-friendly way to access account-related actions.

---
+++ client/src/components/user-avatar.tsx
@@ -0,0 +1,58 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import { IAvatar } from "@/interface/index"
+import { useNavigate } from "react-router-dom"
+
+export const UserAvatar = ({ image, initials }: IAvatar) => {
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        navigate('/')
+    }
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                    <AvatarImage src={image} />
+                    <AvatarFallback>
+                        {initials}
+                    </AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        Profile
+                        {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        Settings
+                        {/* <DropdownMenuShortcut>⌘S</DropdownMenuShortcut> */}
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>GitHub</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={handleLogout}>
+                    Log out
+                    {/* <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut> */}
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
