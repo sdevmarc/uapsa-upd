df46d5e5f96442b48724fc4db845537fffa44a05
sdevmarc <suanieeee@yahoo.com>
2024-08-27 21:28:10 +0800

    feat: Remove AddQr page from routes.tsx

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,13 +2,11 @@ import { createBrowserRouter } from "react-router-dom"
 import Home from "@/pages/Home"
 import SignIn from "./pages/SignIn"
 import Dashboard from "./pages/Dashboard"
-import AddQr from "./pages/Add/add-qr"
 
 const Routes = createBrowserRouter([
     { path: '/', element: <Home /> },
     { path: '/signin', element: <SignIn /> },
     { path: '/dashboard', element: <Dashboard /> },
-    { path: '/add-qr', element: <AddQr /> },
 ])
 
 export default Routes
