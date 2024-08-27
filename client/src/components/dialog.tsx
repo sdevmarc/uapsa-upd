2659db374c81bda9f2898601b0a728b3e9e7a9fb
sdevmarc <suanieeee@yahoo.com>
2024-08-27 21:44:32 +0800

    feat: Update DialogContainer component to accept dynamic title, description, and children
    Update the DialogContainer component in dialog.tsx to accept dynamic title, description, and children props. This change allows for more flexibility in customizing the content of the dialog, improving the reusability and versatility of the component.

--- client/src/components/dialog.tsx
+++ client/src/components/dialog.tsx
@@ -8,11 +8,9 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import React from "react"
 
-export function DialogContainer({ Trigger }: { Trigger: React.ReactNode}) {
+export function DialogContainer({ Trigger, title, description, children }: { Trigger: React.ReactNode, title: string, description: string, children: React.ReactNode }) {
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -20,27 +18,20 @@ export function DialogContainer({ Trigger }: { Trigger: React.ReactNode}) {
             </DialogTrigger>
             <DialogContent className="sm:max-w-[425px]">
                 <DialogHeader>
-                    <DialogTitle>Edit profile</DialogTitle>
+                    <DialogTitle>
+                        {title}
+                    </DialogTitle>
                     <DialogDescription>
-                        Make changes to your profile here. Click save when you're done.
+                        {description}
                     </DialogDescription>
                 </DialogHeader>
                 <div className="grid gap-4 py-4">
                     <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
-                            Name
-                        </Label>
-                        <Input id="name" value="Pedro Duarte" className="col-span-3" />
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
-                            Username
-                        </Label>
-                        <Input id="username" value="@peduarte" className="col-span-3" />
+                        {children}
                     </div>
                 </div>
                 <DialogFooter>
-                    <Button type="submit">Save changes</Button>
+                    <Button type="submit">Submit</Button>
                 </DialogFooter>
             </DialogContent>
         </Dialog>
