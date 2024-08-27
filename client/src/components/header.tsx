21e99279c4bece51e8dd37f3bcaa6549f22f6d16
sdevmarc <suanieeee@yahoo.com>
2024-08-27 20:03:35 +0800

    feat: Improve header design and add logo
    Update the Header component to improve the design and add a logo. The code changes in header.tsx include adding a new logo image and updating the styling of the header. This enhancement enhances the application by providing a visually appealing and branded header. The commit message follows the established convention of using the "feat" type for new feature additions.

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -12,8 +12,8 @@ export default function Header() {
         <div className="sticky top-0 w-full h-[4rem] flex justify-center items-center border-b bg-background z-[1]">
             <div className="w-full h-full max-w-[90rem] flex justify-between items-center px-4">
                 <nav className='flex gap-2 items-center'>
-                    <Link to={`/`}>
-                        My Little Ponky
+                    <Link to={`/`} className='italic bg-accent px-4 font-bold text-primary-foreground'>
+                        MLP
                     </Link>
                     <Link to={`/`} className='text-md font-medium'>
                         QR
