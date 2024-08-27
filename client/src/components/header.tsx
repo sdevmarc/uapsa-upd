18ee4085b11ed8d6ac8979d265412b241c947095
sdevmarc <suanieeee@yahoo.com>
2024-08-27 21:28:26 +0800

    feat: Update header with logo and user avatar
    Update the header component in header.tsx to include a logo and user avatar. This change replaces the previous implementation of the header with a new design that features the application name "My Little Ponky" and a donkey logo. Additionally, a user avatar is added to the header using the UserAvatar component. These enhancements improve the visual appeal and branding of the application.

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,33 +1,16 @@
-import { Link, useNavigate } from 'react-router-dom'
-import { Button } from './ui/button'
+import { UserAvatar } from './user-avatar'
+import Image from '@/assets/donkey-logo.png'
 
 export default function Header() {
-    const navigate = useNavigate()
-
-    const handleLogout = () => {
-        navigate('/')
-    }
-
     return (
         <div className="sticky top-0 w-full h-[4rem] flex justify-center items-center border-b bg-background z-[1]">
             <div className="w-full h-full max-w-[90rem] flex justify-between items-center px-4">
                 <nav className='flex gap-2 items-center'>
-                    <Link to={`/`} className='italic bg-accent px-4 font-bold text-primary-foreground'>
-                        MLP
-                    </Link>
-                    <Link to={`/`} className='text-md font-medium'>
-                        QR
-                    </Link>
-                    <Link to={`/`} className='text-md font-medium'>
-                        Attendance
-                    </Link>
-                    <Link to={`/`} className='text-md font-medium'>
-                        Management
-                    </Link>
+                    <h1 className='px-4 font-semibold text-primary flex items-center gap-4'>
+                        My Little Ponky <img src={Image} alt="donkey" className='w-[1rem] h-full object-contain'/>
+                    </h1>
                 </nav>
-                <Button onClick={handleLogout} variant={`default`}>
-                    Logout
-                </Button>
+                <UserAvatar image='https://github.com/shadcn.png' initials='CN' />
             </div>
         </div>
     )
