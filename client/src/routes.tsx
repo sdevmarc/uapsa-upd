7d8d67908bff7ea9361a260297a5eb73df07f8b8
sdevmarc <suanieeee@yahoo.com>
2024-08-27 19:29:28 +0800

    feat: Add SignIn and Dashboard pages
    This commit adds the SignIn and Dashboard pages to the application. The code changes in routes.tsx include the necessary routes for these new pages. This feature enhances the application by providing a login functionality and a dashboard for authenticated users. The commit message follows the established convention of using the "feat" type for new feature additions.

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,8 +1,12 @@
 import { createBrowserRouter } from "react-router-dom"
 import Home from "@/pages/Home"
+import SignIn from "./pages/SignIn"
+import Dashboard from "./pages/Dashboard"
 
 const Routes = createBrowserRouter([
-    { path: '/', element: <Home /> }
+    { path: '/', element: <Home /> },
+    { path: '/signin', element: <SignIn /> },
+    { path: '/dashboard', element: <Dashboard /> },
 ])
 
 export default Routes
