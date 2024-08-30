adf7d0faa0eae8dc080b7cb28ffd0e432d7b9d0f
sdevmarc <suanieeee@yahoo.com>
2024-08-30 23:02:04 +0800

    feat: Update API_SIGN_IN and API_SIGN_UP functions
    Refactor the API_SIGN_IN and API_SIGN_UP functions in the api/index.ts file to use async/await syntax for improved readability and error handling. This change ensures that the functions correctly handle API requests for user authentication and registration, providing a more robust and reliable user experience.

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,9 +1,27 @@
+import { HOST } from '@/constants'
 import axios from 'axios'
 
-export const API_SIGN_IN = () => {
+interface IUser {
+    email: string
+    password: string
+}
+
+export const API_SIGN_IN = async ({ email, password }: IUser) => {
     try {
-const data = 
+        const response = await axios.post(`${HOST}/users/login-user`, { email, password })
+        return response.data
     } catch (error) {
-        console.error(error)
+        throw error
+    }
+}
+
+export const API_SIGN_UP = async ({ idNumber, name, degree, email, password }: { idNumber: string, name: string, degree: string, email: string, password: string }) => {
+    try {
+        const response = await axios.post(`${HOST}/users/create-first-time-user`,
+            { idNumber, name, degree, email, password }
+        )
+        return response.data
+    } catch (error) {
+        throw error
     }
 }
