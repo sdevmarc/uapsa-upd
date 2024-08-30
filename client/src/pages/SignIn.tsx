3b5e5be4580142a94c1f3dc220bb31a466e7ab75
sdevmarc <suanieeee@yahoo.com>
2024-08-30 23:03:29 +0800

    feat: Add sign-in functionality to SignIn page component
    This commit adds sign-in functionality to the SignIn page component. It allows users to enter their email and password, and upon submission, it sends a request to the server to authenticate the user. If the authentication is successful, the user is redirected to the dashboard page. This change enhances the application by providing a way for users to sign in and access their account.

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,12 +1,36 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { useNavigate } from "react-router-dom"
+import { useMutation } from "@tanstack/react-query"
+import { API_SIGN_IN } from "@/api"
+import { useState } from "react"
 
 export default function SignIn() {
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    })
     const navigate = useNavigate()
 
-    const handleSignIn = () => {
-        navigate('/dashboard')
+    const { mutateAsync: QuerySignInUser, isPending: SignInloading } = useMutation({
+        mutationFn: API_SIGN_IN,
+        onSuccess: (data) => {
+            if (!data.success) return navigate('/signup')
+            if (data.success) return navigate('/dashboard')
+        }
+    })
+
+    const handleSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        QuerySignInUser({ email: values.email, password: values.password })
+    }
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target
+        setValues((prev) => ({
+            ...prev,
+            [name]: value
+        }))
     }
 
     const handleGoBack = () => {
@@ -19,18 +43,18 @@ export default function SignIn() {
                 <form onSubmit={handleSignIn} className="w-[35%] h-full flex flex-col justify-center items-center px-[5rem] gap-4">
                     <h1 className='text-[2rem] font-bold'>Sign In</h1>
                     <div className="w-full flex flex-col gap-1">
-                        <label htmlFor="email" className=''>
+                        <label htmlFor="email" className='text-sm'>
                             Email
                         </label>
-                        <Input placeholder="eg. m@example.com" className="placeholder:text-muted" id="email" required />
+                        <Input placeholder="eg. m@example.com" className="placeholder:text-muted placeholder:text-sm text-sm" id="email" name="email" onChange={handleOnChange} required />
                     </div>
                     <div className="w-full flex flex-col gap-1">
-                        <label htmlFor="password" className=''>
+                        <label htmlFor="password" className="text-sm">
                             Password
                         </label>
-                        <Input id="password" required />
+                        <Input id="password" required name="password" className="text-sm" onChange={handleOnChange} />
                     </div>
-                    <Button type="submit" variant={`default`} className="w-full">
+                    <Button type="submit" variant={`default`} className="w-full" >
                         Sign In
                     </Button>
                     <Button onClick={handleGoBack} type="button" variant={`outline`} className="w-full">
