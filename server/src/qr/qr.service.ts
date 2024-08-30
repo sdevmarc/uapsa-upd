469dbe84c36b14df0e0e501759cc4c0dd4fa26b5
sdevmarc <suanieeee@yahoo.com>
2024-08-31 01:07:40 +0800

    feat: Add bcrypt hashing for QR ID in QrService
    This commit adds bcrypt hashing for the QR ID in the QrService. It uses the bcrypt library to hash the QR ID before storing it in the database. This change enhances the security of the application by protecting the QR ID from being easily compromised.

--- server/src/qr/qr.service.ts
+++ server/src/qr/qr.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { IQr } from './qr.interface';
 import { IAttendance } from 'src/attendance/attendance.interface';
 import { IPoints } from 'src/points/points.interface';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class QrService {
@@ -21,20 +22,24 @@ export class QrService {
     }
 
     async findOne({ qr }: { qr: string })
-        : Promise<{ success: boolean, message: string, data: IQr, attendance: IAttendance, points: IPoints }> {
-
+        : Promise<{ success: boolean, message: string, data?: IQr, attendance?: IAttendance, points?: IPoints }> {
         const data = await this.QrModel.findById(qr)
+        const isqr = await bcrypt.compare(qr, data._id.toString())
+
         const attendance = await this.AttendanceModel.findOne({ qr })
         const points = await this.PointModel.findOne({ qr })
 
-        if (!data) return { success: true, message: 'Cannot find qr user!', data, attendance, points }
+        if (!isqr) return { success: false, message: 'Cannot find qr user!' }
         return { success: true, message: 'Qr user data retrieved successfully!', data, attendance, points }
     }
 
     async InsertQr({ idNumber, name, degree }: IQr)
-        : Promise<{ success: boolean, message: string }> {
-        await this.QrModel.create({ idNumber, name, degree })
-        return { success: true, message: 'Qr created successfully!' }
+        : Promise<{ success: boolean, message: string, qr: string }> {
+        const salt = await bcrypt.genSalt()
+        const hashedqr = await bcrypt.hash(idNumber, salt)
+
+        const data = await this.QrModel.create({ idNumber: hashedqr, name, degree })
+        return { success: true, message: 'Qr created successfully!', qr: data._id.toString() }
     }
 
     async DeleteQr({ idNumber }: { idNumber: string })
