7c37ac009db65d2cef41e06f72b23eff6a2b0678
Marc Edison <suanieeee@yahoo.com>
2024-09-01 03:54:54 +0800

    refactor: Add QR and Management schemas

---
+++ client/src/components/data-table-components/schema.tsx
@@ -0,0 +1,14 @@
+export interface IQRSchema {
+    _id?: string
+    idNumber: string
+    name: string
+    degree: string
+    attended: string
+    points: string
+}
+
+export interface IManagementSchema {
+    _id?: string
+    email: string
+    role: string
+}
