392059e7a5cf51afeb16d769a1534bb91f1a570d
sdevmarc <suanieeee@yahoo.com>
2024-08-31 16:01:17 +0800

    feat: Implement lazy loading for images in Dashboard page

--- server/src/qr/qr.service.ts
+++ server/src/qr/qr.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import mongoose, { Model } from 'mongoose';
+import { Model } from 'mongoose';
 import { IQr } from './qr.interface';
 import { IAttendance } from 'src/attendance/attendance.interface';
 import { IPoints } from 'src/points/points.interface';
-import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class QrService {
@@ -16,7 +15,61 @@ export class QrService {
 
     async findAll()
         : Promise<{ success: boolean, message: string, data: IQr[] }> {
-        const data = await this.QrModel.find()
+        const data = await this.QrModel.aggregate([
+            {
+                $lookup: {
+                    from: 'qrs', // The name of the QR collection
+                    localField: 'qr',
+                    foreignField: '_id',
+                    as: 'qrData'
+                }
+            },
+            {
+                $unwind: {
+                    path: '$qrData',
+                    preserveNullAndEmptyArrays: true
+                }
+            },
+            {
+                $lookup: {
+                    from: 'points', // The name of the Points collection
+                    localField: 'qr',
+                    foreignField: 'qr',
+                    as: 'pointsData'
+                }
+            },
+            {
+                $unwind: {
+                    path: '$pointsData',
+                    preserveNullAndEmptyArrays: true
+                }
+            },
+            {
+                $lookup: {
+                    from: 'attendances', // The name of the Attendance collection
+                    localField: 'qr',
+                    foreignField: 'qr',
+                    as: 'attendanceData'
+                }
+            },
+            {
+                $unwind: {
+                    path: '$attendanceData',
+                    preserveNullAndEmptyArrays: true
+                }
+            },
+            {
+                $project: {
+                    idNumber: 1,
+                    name: 1,
+                    degree: 1,
+                    points: { $ifNull: ['$pointsData.points', 0] },
+                    attended: { $ifNull: ['$attendanceData.attended', 0] }
+                }
+            }
+        ])
+
+
         if (data.length <= 0) return { success: true, message: 'No exisisting qr users!', data }
         return { success: true, message: 'Qr users retrieved successfully!', data }
     }
@@ -24,21 +77,18 @@ export class QrService {
     async findOne({ qr }: { qr: string })
         : Promise<{ success: boolean, message: string, data?: IQr, attendance?: IAttendance, points?: IPoints }> {
         const data = await this.QrModel.findById(qr)
-        const isqr = await bcrypt.compare(qr, data._id.toString())
 
         const attendance = await this.AttendanceModel.findOne({ qr })
         const points = await this.PointModel.findOne({ qr })
 
-        if (!isqr) return { success: false, message: 'Cannot find qr user!' }
+        if (!data) return { success: false, message: 'Cannot find qr user!' }
         return { success: true, message: 'Qr user data retrieved successfully!', data, attendance, points }
     }
 
     async InsertQr({ idNumber, name, degree }: IQr)
         : Promise<{ success: boolean, message: string, qr: string }> {
-        const salt = await bcrypt.genSalt()
-        const hashedqr = await bcrypt.hash(idNumber, salt)
 
-        const data = await this.QrModel.create({ idNumber: hashedqr, name, degree })
+        const data = await this.QrModel.create({ idNumber: idNumber, name, degree })
         return { success: true, message: 'Qr created successfully!', qr: data._id.toString() }
     }
 
