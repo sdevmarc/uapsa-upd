4bc4ab4d5de8473862d05582719d7db2c8d4759f
Marc Edison <suanieeee@yahoo.com>
2024-09-01 03:55:12 +0800

    refactor: Update Management page with user management data table
    This refactor updates the Management page to include a user management data table. It adds the necessary components and queries to fetch and display user management data. This enhancement improves the functionality of the application by providing a way to manage registered system user access.

--- client/src/pages/Management.tsx
+++ client/src/pages/Management.tsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import { DataTable } from "@/components/data-table-components/data-table";
+import Header from "@/components/header";
+import { useEffect } from "react";
+import HeadSection, { SubHeadSectionDetails } from "@/components/head-section";
+import { useQuery } from "@tanstack/react-query";
+import { API_DATA_USER_MANAGEMENT, API_INDEX } from "@/api";
+import { Link, useNavigate } from "react-router-dom"
+import { managementcolumns } from "@/components/data-table-components/columns/management-columns";
 
 export default function Management() {
-  return (
-    <div>Management</div>
-  )
+    const navigate = useNavigate()
+    const token = localStorage.getItem('token')
+
+    useEffect(() => {
+        if (!token) { navigate('/') }
+    }, [token, navigate]);
+
+    const { data: jwtAuthorized, isFetched: jwtFetched, isLoading: jwtLoading } = useQuery({
+        queryFn: () => API_INDEX({ token: token ?? '' }),
+        queryKey: ['dashboardJwt', { token: token ?? '' }],
+        enabled: !!token
+    })
+
+    const { data: usermanagement = [], isLoading: usermanagementLoading, isFetched: usermanagementFetched } = useQuery({
+        queryFn: () => API_DATA_USER_MANAGEMENT({ token: token ?? '' }),
+        queryKey: ['dashboardUserManagement', { token: token ?? '' }],
+        enabled: !!token
+    })
+
+    console.log(usermanagement)
+
+    useEffect(() => {
+        if (jwtFetched && !jwtAuthorized) { navigate('/') }
+    }, [jwtFetched, jwtAuthorized, navigate]);
+
+    return (
+        <>
+            <div className="w-full flex flex-col justify-center items-center">
+                <Header>
+                    <Link to={'/dashboard'} className="text-sm hover:underline">
+                        Go Back
+                    </Link>
+                </Header>
+                <div className="w-full max-w-[90rem] px-4 flex flex-col gap-4">
+                    <HeadSection>
+                        <SubHeadSectionDetails
+                            title="RECORD OF REGISTERED USER ACCESS"
+                            description="Here's a list of registered system user access."
+                        />
+                    </HeadSection>
+                    {(jwtLoading || usermanagementLoading) && 'Loading...'}
+                    {(jwtFetched && usermanagementFetched) &&
+                        <DataTable columns={managementcolumns} data={usermanagement.data} toolbar="management" />
+                    }
+                </div>
+            </div>
+        </>
+    )
 }
