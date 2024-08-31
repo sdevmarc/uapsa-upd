c19c674865baf6869a497605fda133134d7b331a
sdevmarc <suanieeee@yahoo.com>
2024-08-31 20:01:28 +0800

    feat: Add QR code generation and download functionality to DataTableToolbar component

--- client/src/components/data-table-components/data-table-toolbar.tsx
+++ client/src/components/data-table-components/data-table-toolbar.tsx
@@ -3,14 +3,18 @@
 import { Cross2Icon } from "@radix-ui/react-icons";
 import { Table } from "@tanstack/react-table";
 
+import QRCode from 'qrcode';
 import { Button } from "@/components/ui/button";
 import { DataTableViewOptions } from "@/components/data-table-components/data-table-view-options";
 import { DialogContainer } from "../dialog";
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { API_CREAT_QR } from "@/api";
+import { DatePickerAtendance } from "../date-picker";
+import { AlertDialogConfirmation } from "../alert-dialog";
+import { useNavigate } from "react-router-dom";
 
 interface DataTableToolbarProps<TData> {
     table: Table<TData>;
@@ -22,11 +26,20 @@ interface IQr {
     degree: string
 }
 
+const now = new Date();
+const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-based, so add 1
+const day = String(now.getDate()).padStart(2, '0');
+const year = now.getFullYear();
+
+const formattedDate = `${month}/${day}/${year}`;
+
 export function DataTableToolbar<TData>({
     table
 }: DataTableToolbarProps<TData>) {
+    const navigate = useNavigate()
     const queryClient = useQueryClient();
     const isFiltered = table.getState().columnFilters.length > 0;
+    const canvasRef = useRef<HTMLCanvasElement>(null);
     const [qrvalues, setQrValues] = useState<IQr>({
         idNumber: '',
         name: '',
@@ -41,9 +54,10 @@ export function DataTableToolbar<TData>({
         }
     })
 
-    const handleAddQr = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleAddQr = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        InsertQr(qrvalues)
+        await InsertQr(qrvalues)
+        await downloadQRCode({ idNumber: qrvalues.idNumber })
     }
 
     const handleQrChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -54,8 +68,26 @@ export function DataTableToolbar<TData>({
         }))
     }
 
+    const downloadQRCode = async ({ idNumber }: { idNumber: string }) => {
+        if (canvasRef.current) {
+            try {
+                await QRCode.toCanvas(canvasRef.current, idNumber, { width: 500 });
+                const image = canvasRef.current.toDataURL("image/png");
+                const link = document.createElement('a');
+                link.href = image;
+                link.download = `qrcode-${idNumber}.png`;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            } catch (error) {
+                console.error("Error generating QR code:", error);
+            }
+        }
+    };
+
     return (
         <div className="flex flex-wrap items-center justify-between">
+            <canvas ref={canvasRef} style={{ display: 'none' }} />
             <div className="flex flex-1 flex-wrap items-center gap-2">
                 <Input
                     placeholder="Search name..."
@@ -104,24 +136,19 @@ export function DataTableToolbar<TData>({
                     }
 
                 />
-                {/* <DialogContainer
-                    title="Add Attendance"
-                    description="Please fill-out the required fields."
-                    Trigger={
-                        <Button variant={`outline`} size={`sm`}>
-                            Add Attendance
-                        </Button>
-                    }
-                    children={
-                        <>
-                            <div className="w-full">
-                                <DatePickerAtendance />
-                            </div>
-
-                        </>
-                    }
+                <AlertDialogConfirmation
+                    btnTitle="Add Attendance"
+                    title="Are you sure?"
+                    description={`This will create a new attendance at ${formattedDate}`}
+                    btnContinue={() => navigate('/scanattendance')}
                 />
-                <DialogContainer
+                <AlertDialogConfirmation
+                    btnTitle="Add Point"
+                    title="Are you sure?"
+                    description={`This will direct you to a qr scanner to earn points.`}
+                    btnContinue={() => navigate('/scanpoints')}
+                />
+                {/*  <DialogContainer
                     title="Add Point"
                     description="Please fill-out the required fields."
                     Trigger={
