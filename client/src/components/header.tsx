79affe7321d43b8ade487169ba0000d68a3026f9
Marc Edison <suanieeee@yahoo.com>
2024-09-01 00:23:10 +0800

    refactor: Improve header component structure and styling
    Refactor the header component to improve its structure and styling. This update includes changes to the HTML structure and CSS classes to enhance the overall layout and visual appearance of the header. By improving the header component, we ensure a consistent and visually appealing design across the application.

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,14 +1,16 @@
+import React from 'react'
 import { UserAvatar } from './user-avatar'
 import Image from '@/assets/donkey-logo.png'
 
-export default function Header() {
+export default function Header({ children }: { children?: React.ReactNode }) {
     return (
-        <div className="fixed top-0 w-full h-[4rem] flex justify-center items-center border-b bg-background z-[1]">
+        <div className="z-[2] fixed top-0 w-full h-[4rem] flex justify-center items-center border-b bg-background">
             <div className="w-full h-full max-w-[90rem] flex justify-between items-center px-4">
                 <nav className='flex gap-2 items-center'>
                     <h1 className='px-4 font-semibold text-primary flex items-center gap-4'>
-                        My Little Ponky <img src={Image} alt="donkey" className='w-[1rem] h-full object-contain'/>
+                        My Little Ponky <img src={Image} alt="donkey" className='w-[1rem] h-full object-contain' />
                     </h1>
+                    {children}
                 </nav>
                 <UserAvatar image='https://github.com/shadcn.png' initials='CN' />
             </div>
