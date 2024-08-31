8f493ea40ca5c3da2379940a565af29f2001814b
sdevmarc <suanieeee@yahoo.com>
2024-08-31 20:01:18 +0800

    refactor: Update Header component positioning
    Update the positioning of the Header component from "sticky" to "fixed" to ensure it remains fixed at the top of the page even when scrolling. This change improves the user experience by providing a consistent header navigation throughout the application.

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -3,7 +3,7 @@ import Image from '@/assets/donkey-logo.png'
 
 export default function Header() {
     return (
-        <div className="sticky top-0 w-full h-[4rem] flex justify-center items-center border-b bg-background z-[1]">
+        <div className="fixed top-0 w-full h-[4rem] flex justify-center items-center border-b bg-background z-[1]">
             <div className="w-full h-full max-w-[90rem] flex justify-between items-center px-4">
                 <nav className='flex gap-2 items-center'>
                     <h1 className='px-4 font-semibold text-primary flex items-center gap-4'>
