c9c31a5d291fa3d952be96ce392b70e73eea4d70
Marc Edison <suanieeee@yahoo.com>
2024-09-01 03:55:09 +0800

    refactor: Update Home page with new design and lazy loading for images

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@/components/ui/button'
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import BlurImage from '@/assets/BlurBlob.png'
 
 export default function Home() {
     const navigate = useNavigate()
@@ -12,27 +12,23 @@ export default function Home() {
 
     return (
         <>
-            <div className="w-full h-screen flex flex-col justify-start items-center">
-                <div className="w-full max-w-[90rem] px-4">
-                    <Header />
+            <div className="relative overflow-hidden bg-background w-full h-screen flex flex-col justify-start items-center">
+                <img src={BlurImage} alt="Blur Image Asset" className="absolute top-[-5rem] left-[-10rem] w-[20rem] h-[20rem] scale-[4]" />
+                <img src={BlurImage} alt="Blur Image Asset" className="absolute top-[-5rem] right-[-10rem] w-[20rem] h-[20rem] scale-[4]" />
+                <img src={BlurImage} alt="Blur Image Asset" className="absolute top-[50rem] left-[30rem] w-[20rem] h-[20rem] scale-[4]" />
+                <div className="z-[1] w-full h-full max-w-[90rem] px-4">
+                    <main className="w-full h-full flex flex-col justify-center items-center gap-4">
+                        <h1 className='text-center text-[4rem] sm:text-[4rem] md:text-[4rem] lg:text-[5rem] xl:text-[5rem] font-semibold text-primary'>My Little Ponky</h1>
+                        <p className='text-sm text-primary text-center'>
+                            Track attendance and points effortlessly with a click. Simple, smart, and always on top!
+                            </p>
+                        <Link to={`/signin`} className='text-sm text-primary px-8 py-2 hover:bg-accent hover:border-none rounded-full duration-300 ease-in-out border-[2px] border-black/20 dark:border-white/20'>
+                            Sign In
+                        </Link>
+                    </main>
                 </div>
+
             </div>
         </>
-    )
-}
-
-const Header = () => {
-    const navigate = useNavigate()
-
-    const handleSignIn = () => {
-        navigate('/signin')
-    }
-
-    return (
-        <header className="w-full h-[4rem] flex justify-end items-center">
-            <Button onClick={handleSignIn} variant={`default`}>
-                Sign In
-            </Button>
-        </header>
     )
 }
