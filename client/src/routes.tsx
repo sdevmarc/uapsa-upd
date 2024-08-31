96ec2b199e0590224464527e0fa1149cffe60455
sdevmarc <suanieeee@yahoo.com>
2024-08-31 20:01:05 +0800

    feat: Add ScanAttendance and ScanPoints pages
    Add new pages, ScanAttendance and ScanPoints, to handle attendance scanning and point scanning functionality. These pages provide a seamless user experience for scanning attendance and points using QR codes. This feature enhances the functionality and usability of the application.

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -7,12 +7,15 @@ import Management from "./pages/Management"
 import Settings from "./pages/Settings"
 import SignUp from "./pages/SignUp"
 import Home from "./pages/Home"
+import ScanAttendance from "./pages/ScanAttendance"
 
 const Routes = createBrowserRouter([
     { path: '/', element: <Home /> },
     { path: '/signin', element: <SignIn /> },
     { path: '/signup', element: <SignUp /> },
     { path: '/dashboard', element: <Dashboard /> },
+    { path: '/scanattendance', element: <ScanAttendance /> },
+    { path: '/scanpoints', element: <ScanPoints /> },
     { path: '/attendance', element: <Attendance /> },
     { path: '/management', element: <Management /> },
     { path: '/settings', element: <Settings /> },
