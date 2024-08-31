134469430c12e42bdeb9f259249215fb81a40a86
Marc Edison <suanieeee@yahoo.com>
2024-09-01 00:22:59 +0800

    feat: Add Toaster component for displaying notifications

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider } from 'react-router-dom'
 import Routes from './routes'
 import { ThemeProvider } from './hooks/useTheme'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from "@/components/ui/sonner"
 
 const queryClient = new QueryClient()
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
             <QueryClientProvider client={queryClient}>
                 <RouterProvider router={Routes} />
+                <Toaster />
             </QueryClientProvider>
         </ThemeProvider>
     </React.StrictMode>,
