195fe41a4be336a892598e5f9bd23f648e182357
sdevmarc <suanieeee@yahoo.com>
2024-09-01 18:33:27 +0800

    feat: Add ID No., Name, and Course columns to management table
    The code changes in this commit add three new columns to the management table: ID No., Name, and Course. These columns display the corresponding data from the management schema. This enhancement improves the visibility and organization of the table, providing more comprehensive information for each entry.

--- client/src/components/data-table-components/columns/management-columns.tsx
+++ client/src/components/data-table-components/columns/management-columns.tsx
@@ -2,10 +2,49 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "@/components/data-table-components/data-table-column-header";
-import { DataTableRowActions } from "@/components/data-table-components/data-table-row-actions";
 import { IManagementSchema } from "../schema";
+import { DataTableRowActionsManagement } from "@/components/row-actions/data-table-row-actions-management";
 
 export const managementcolumns: ColumnDef<IManagementSchema>[] = [
+    {
+        accessorKey: "idNumber",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="ID No." className="text-text" />
+        ),
+        cell: ({ row }) => (
+            <div className="w-[80px] capitalize">{row.getValue("idNumber")}</div>
+        ),
+        enableSorting: false,
+        enableHiding: false
+    },
+    {
+        accessorKey: "name",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Name" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex space-x-2">
+                    <span className="max-w-[500px] truncate capitalize font-medium">
+                        {row.getValue("name")}
+                    </span>
+                </div>
+            );
+        }
+    },
+    {
+        accessorKey: "degree",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Course" className="text-text" />
+        ),
+        cell: ({ row }) => {
+            return (
+                <div className="flex w-[100px] items-center">
+                    <span className="capitalize"> {row.getValue("degree")}</span>
+                </div>
+            );
+        },
+    },
     {
         accessorKey: "email",
         header: ({ column }) => (
@@ -26,16 +65,14 @@ export const managementcolumns: ColumnDef<IManagementSchema>[] = [
         ),
         cell: ({ row }) => {
             return (
-                <div className="flex space-x-2">
-                    <span className="max-w-[500px] truncate capitalize font-medium">
-                        {row.getValue("role")}
-                    </span>
+                <div className="flex w-[100px] items-center">
+                    <span className="capitalize"> {row.getValue("role")}</span>
                 </div>
             );
-        }
+        },
     },
     {
         id: "actions",
-        cell: ({ row }) => <DataTableRowActions row={row} />
+        cell: ({ row }) => <DataTableRowActionsManagement row={row} />
     }
 ];
