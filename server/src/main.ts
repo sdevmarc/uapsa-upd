e65bb7f0024c341f421349eaf4bbc01cb5463372
sdevmarc <suanieeee@yahoo.com>
2024-09-01 20:29:20 +0800

    ```text refactor: Update server main.ts to listen on default port if PORT environment variable is not set
    The commit updates the server's main.ts file to listen on the default port (3003) if the PORT environment variable is not set. This change ensures that the server can still run and accept incoming requests even if the PORT variable is not explicitly defined.

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalGuards(new AuthGuard(app.get(JwtService)));
   app.enableCors();
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3003);
 }
 
 bootstrap()
