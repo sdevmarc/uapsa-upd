5801734f5da88e357d9b67bb5d01afe491d8e35a
sdevmarc <suanieeee@yahoo.com>
2024-09-01 18:34:21 +0800

    feat: Add Sign In page for user authentication
    This commit adds the Sign In page, which allows users to authenticate by entering their email and password. It includes form validation, API integration for signing in, and navigation to the dashboard upon successful authentication. This feature enhances the functionality of the application by providing a dedicated page for user authentication.

---
+++ client/src/pages/sign-in.tsx
@@ -0,0 +1,117 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useNavigate } from "react-router-dom"
+import { useMutation } from "@tanstack/react-query"
+import { API_SIGN_IN } from "@/api"
+import { useState } from "react"
+import { Eye, EyeOff } from "lucide-react"
+import bgImage from '@/assets/sapiens.svg'
+import { toast } from "sonner"
+import ScreenLoading from "@/components/screen-loading"
+
+export default function SignIn() {
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    })
+    const [showPassword, setShowPassword] = useState(false)
+    const navigate = useNavigate()
+
+    const { mutateAsync: QuerySignInUser, isPending: SignInloading } = useMutation({
+        mutationFn: API_SIGN_IN,
+        onSuccess: (data) => {
+            if (!data.success) {
+                toast("Oops, Something went wrong!", { description: 'There are no registered user yet!' })
+                return navigate('/signup')
+            }
+            localStorage.setItem('token', data.access_token)
+            return navigate('/dashboard')
+        },
+        onError: () => {
+            toast("Oops, something went wrong!",  {  description: 'Please check your credentials, try again!'  })
+        }
+    })
+
+    const handleSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        QuerySignInUser({ email: values.email, password: values.password })
+    }
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target
+        setValues((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const handleGoBack = () => {
+        navigate('/')
+    }
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+
+    return (
+        <>
+            <div className="w-full h-screen flex justify-center items-center">
+                {SignInloading && <ScreenLoading />}
+                <form onSubmit={handleSignIn} className="w-[35%] h-full flex flex-col justify-center items-center px-[5rem] gap-4">
+                    <h1 className='text-[2rem] font-bold'>Sign In</h1>
+                    <div className="w-full flex flex-col gap-1">
+                        <label htmlFor="email" className='text-sm'>
+                            Email
+                        </label>
+                        <Input
+                            type="email"
+                            placeholder="eg. m@example.com"
+                            className="placeholder:text-muted placeholder:text-sm text-sm"
+                            id="email"
+                            name="email"
+                            onChange={handleOnChange}
+                            required
+                        />
+                    </div>
+                    <div className="w-full flex flex-col gap-1">
+                        <label htmlFor="password" className="text-sm">
+                            Password
+                        </label>
+                        <div className="relative">
+                            <Input
+                                id="password"
+                                required
+                                name="password"
+                                className="text-sm pr-10"
+                                onChange={handleOnChange}
+                                type={showPassword ? "text" : "password"}
+                            />
+                            <button
+                                type="button"
+                                className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                                onClick={toggleShowPassword}
+                            >
+                                {showPassword ? (
+                                    <EyeOff className="h-4 w-4 text-gray-400" />
+                                ) : (
+                                    <Eye className="h-4 w-4 text-gray-400" />
+                                )}
+                            </button>
+                        </div>
+                    </div>
+                    <Button type="submit" variant="default" className="w-full" disabled={SignInloading}>
+                        {SignInloading ? 'Signing In...' : 'Sign In'}
+                    </Button>
+                    <Button onClick={handleGoBack} type="button" variant="outline" className="w-full">
+                        Go back
+                    </Button>
+                </form>
+                <div className="w-[65%] h-full flex justify-center items-center">
+                    <div className="w-[80%] h-[80%] flex justify-center items-center">
+                        <img src={bgImage} alt="Image Background" className="object-cover w-full h-full" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
