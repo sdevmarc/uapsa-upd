d734b1b7303bb3319160f44bf876b844949aa0e0
sdevmarc <suanieeee@yahoo.com>
2024-09-04 21:08:55 +0800

    feat: Add QR model to PointsService

--- server/src/points/points.service.ts
+++ server/src/points/points.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IPoints } from './points.interface';
+import { IQr } from 'src/qr/qr.interface';
 
 @Injectable()
 export class PointsService {
     constructor(
-        @InjectModel('Point')
-        private readonly PointModel: Model<IPoints>
+        @InjectModel('Point') private readonly PointModel: Model<IPoints>,
+        @InjectModel('Qr') private readonly QrModel: Model<IQr>,
     ) { }
 
     async InsertPoint({ qr }: { qr: string })
         : Promise<{ success: boolean, message: string }> {
+        const isqr = await this.QrModel.findById(qr)
+        if (!isqr) return { success: false, message: 'Qr is not registered.' }
         await this.PointModel.findOneAndUpdate(
             { qr },
             { $inc: { points: 1 } },
@@ -22,6 +25,8 @@ export class PointsService {
 
     async LessPoint({ qr }: { qr: string })
         : Promise<{ success: boolean, message: string }> {
+        const isqr = await this.QrModel.findById(qr)
+        if (!isqr) return { success: false, message: 'Qr is not registered.' }
         await this.PointModel.findOneAndUpdate(
             { qr },
             { $inc: { points: -1 } },
