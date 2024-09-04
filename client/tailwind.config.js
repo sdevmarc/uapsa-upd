b5f20780b54443b19c4434c4a3c1b1e55828635e
sdevmarc <suanieeee@yahoo.com>
2024-09-04 21:06:27 +0800

    refactor: Add 'Fredoka One' font to the project
    The commit adds the 'Fredoka One' font to the project by extending the 'fontFamily' configuration in the 'tailwind.config.js' file. This change ensures that the correct font is used in the client application, improving the visual appearance and user experience.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
         extend: {
             fontFamily: {
                 poppins: ['Poppins', 'sans-serif'],
+                fredoka: ['Fredoka One']
             },
             colors: {
                 border: "hsl(var(--border))",
