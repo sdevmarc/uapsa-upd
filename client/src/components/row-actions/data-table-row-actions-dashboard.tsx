bd8d95741cff6b4e3d7bc5339db94f81b1ba739e
sdevmarc <suanieeee@yahoo.com>
2024-09-04 21:07:22 +0800

    refactor: Add delete functionality to DataTableRowActionsDashboard component
    The commit adds the delete functionality to the DataTableRowActionsDashboard component in the client/src/components/row-actions/data-table-row-actions-dashboard.tsx file. It introduces a new handleDelete function that uses the useMutation hook to delete a QR code. This enhancement allows users to delete QR codes directly from the data table, improving the overall user experience and providing more control over the data.

--- client/src/components/row-actions/data-table-row-actions-dashboard.tsx
+++ client/src/components/row-actions/data-table-row-actions-dashboard.tsx
@@ -11,15 +11,27 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
+import { API_DELETE_QR } from "@/api";
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import ScreenLoading from "../screen-loading";
 
 interface DataTableRowActionsProps<TData> {
     row: Row<TData>;
 }
 
 export function DataTableRowActionsDashboard<TData>({ row }: DataTableRowActionsProps<TData>) {
+    const navigate = useNavigate()
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const queryClient = useQueryClient();
+    const token = localStorage.getItem('token') ?? '';
+
+    useEffect(() => {
+        if (!token) { navigate('/') }
+    }, [token, navigate]);
 
     const downloadQRCode = async () => {
         if (canvasRef.current) {
@@ -40,27 +52,44 @@ export function DataTableRowActionsDashboard<TData>({ row }: DataTableRowActions
         }
     };
 
+    const { mutateAsync: deleteQr, isPending: deleteqrLoading } = useMutation({
+        mutationFn: API_DELETE_QR,
+        onSuccess: (data) => {
+            if (!data.success) return toast("Uh oh! something went wrong.", { description: 'Qr failed to delete.' })
+            queryClient.invalidateQueries({ queryKey: ['dashboardQr'] })
+            return toast("Yay! Success.", { description: data.message })
+        }
+    })
+
+    const handleDelete = async () => {
+        const qr = (row.original as any)._id
+        await deleteQr({ qr, token })
+    }
+
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button
-                    variant="ghost"
-                    className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-                >
-                    <DotsHorizontalIcon className="h-4 w-4" />
-                    <span className="sr-only">Open menu</span>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-[160px]">
-                <DropdownMenuItem onClick={downloadQRCode}>
-                    Download Qr
-                </DropdownMenuItem>
-                <canvas ref={canvasRef} style={{ display: 'none' }} />
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    Delete
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <>
+            {deleteqrLoading && <ScreenLoading />}
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant="ghost"
+                        className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+                    >
+                        <DotsHorizontalIcon className="h-4 w-4" />
+                        <span className="sr-only">Open menu</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-[160px]">
+                    <DropdownMenuItem onClick={downloadQRCode}>
+                        Download Qr
+                    </DropdownMenuItem>
+                    <canvas ref={canvasRef} style={{ display: 'none' }} />
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem onClick={handleDelete}>
+                        Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
     );
 }
