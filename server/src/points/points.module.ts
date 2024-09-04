e16ee77c25f87e748a5c5cceb0ea4e6ddcf8e297
sdevmarc <suanieeee@yahoo.com>
2024-09-04 21:08:50 +0800

    refactor: Add QRSchema to Points module
    The commit adds the QRSchema to the Points module in the server/src/points/points.module.ts file. This schema is imported to enable the usage of QR codes in the points functionality of the application. This enhancement improves the functionality and extensibility of the codebase.

--- server/src/points/points.module.ts
+++ server/src/points/points.module.ts
@@ -3,9 +3,13 @@ import { PointsService } from './points.service';
 import { PointsController } from './points.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PointsSchema } from './points.schema';
+import { QRSchema } from 'src/qr/qr.schema';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'Point', schema: PointsSchema }])],
+    imports: [MongooseModule.forFeature([
+        { name: 'Point', schema: PointsSchema },
+        { name: 'Qr', schema: QRSchema },
+    ])],
     providers: [PointsService],
     controllers: [PointsController]
 })
