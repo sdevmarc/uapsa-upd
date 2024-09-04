a47c1d1e5b0fa7e50526b794c868be6e433ba324
sdevmarc <suanieeee@yahoo.com>
2024-09-04 21:08:34 +0800

    refactor: Add QRSchema to Attendance module
    The commit adds the QRSchema to the Attendance module in the server/src/attendance/attendance.module.ts file. This schema is imported to enable the usage of QR codes in the attendance functionality of the application. This enhancement improves the functionality and extensibility of the codebase.

--- server/src/attendance/attendance.module.ts
+++ server/src/attendance/attendance.module.ts
@@ -5,12 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AttendanceSchema } from './attendance.schema';
 import { PointsSchema } from 'src/points/points.schema';
 import { PointsService } from 'src/points/points.service';
+import { QRSchema } from 'src/qr/qr.schema';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: 'Attendance', schema: AttendanceSchema },
             { name: 'Point', schema: PointsSchema },
+            { name: 'Qr', schema: QRSchema },
         ]),
     ],
     providers: [AttendanceService, PointsService],
