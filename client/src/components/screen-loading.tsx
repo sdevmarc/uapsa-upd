484d46082622e6b906fb7346e76d3921f90463da
sdevmarc <suanieeee@yahoo.com>
2024-09-04 21:07:05 +0800

    refactor: Improve ScreenLoading component layout and add random loading messages
    The commit refactors the ScreenLoading component in the client/src/components/screen-loading.tsx file. It updates the layout by adding a flex container and adjusting the background color. Additionally, it introduces a new feature by displaying random loading messages using the LoadingMessage component from loading-messages.tsx. This enhancement adds personality and entertainment to the loading process, improving the user experience.

--- client/src/components/screen-loading.tsx
+++ client/src/components/screen-loading.tsx
@@ -1,16 +1,26 @@
 import { waveform } from 'ldrs'
+import { getRandomLoadingMessage } from './loading-messages'
 
 waveform.register()
 
 export default function ScreenLoading() {
     return (
-        <main className='z-[10] fixed top-0 left-0 w-full h-screen bg-black/80 flex justify-center items-center'>
+        <main className='z-[10] fixed top-0 left-0 w-full h-screen bg-white flex flex-col justify-center items-center gap-8 px-4'>
             <l-waveform
                 size="35"
                 stroke="3.5"
                 speed="1"
-                color="white"
+                color="black"
             ></l-waveform>
+            <div className="flex flex-col gap-2">
+                <h1 className='font-medium text-[1rem] text-primary text-center'>
+                    Loading...
+                </h1>
+                <h1 className='font-semibold text-[1.2rem] text-primary text-center'>
+                    {getRandomLoadingMessage()}
+                </h1>
+            </div>
+
         </main>
     )
 }
