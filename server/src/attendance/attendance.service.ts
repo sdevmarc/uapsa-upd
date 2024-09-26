b816554689f16123c0f4acfc21092ad963f4ee4e
sdevmarc <suanieeee@yahoo.com>
2024-09-26 23:46:27 +0800

    Refactor attendance service to handle QR code data retrieval and manipulation

--- server/src/attendance/attendance.service.ts
+++ server/src/attendance/attendance.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IAttendance } from './attendance.interface';
@@ -14,47 +14,77 @@ export class AttendanceService {
 
     async findAll()
         : Promise<{ success: boolean, message: string, data: IAttendance[] }> {
-        const data = await this.AttendanceModel.find()
-        if (data.length <= 0) return { success: true, message: 'No existing attendance.', data }
-        return { success: true, message: 'Attendance retrieved successfully', data }
+        try {
+            const data = await this.AttendanceModel.find()
+            return { success: true, message: 'Attendance retrieved successfully', data }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Qr user data failed to retrieved users.', error }, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 
     async findOne({ qr }: { qr: string })
         : Promise<{ success: boolean, message: string, data: IAttendance }> {
-        const data = await this.AttendanceModel.findOne({ qr })
-        if (!data) return { success: false, message: 'Cannot find attendance!', data }
+        try {
+            const data = await this.AttendanceModel.findOne({ qr })
+            if (!data) return { success: false, message: 'Cannot find attendance!', data }
 
-        return { success: true, message: 'User attendance retrieved successfully!', data }
+            return { success: true, message: 'User attendance retrieved successfully!', data }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Qr user data failed to retrieved a user.', error }, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 
     async InsertAttended({ qr }: IAttendance)
-        : Promise<{ success: boolean, message: string }> {
-        const isqr = await this.QrModel.findById(qr)
-        if (!isqr) return { success: false, message: 'Qr is not registered.' }
-        await this.AttendanceModel.findOneAndUpdate(
-            { qr },
-            { $inc: { attended: 1 } },
-            { new: true, upsert: true }
-        )
+        : Promise<{ success: boolean, message: string, qr?: string }> {
+        try {
+            const decoded_base64 = this.decodeBase64(qr)
 
-        return { success: true, message: 'Attended created sucessfully!' }
+            const isqr = await this.QrModel.findById(decoded_base64)
+            if (!isqr) return { success: false, message: 'Qr is not registered.' }
+            await this.AttendanceModel.findOneAndUpdate(
+                { qr: decoded_base64 },
+                { $inc: { attended: 1 } },
+                { new: true, upsert: true }
+            )
+
+            return { success: true, message: 'Attended created sucessfully!', qr: isqr._id.toString() }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Qr user data failed to insert attendance.', error }, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 
     async InsertAbsent({ qr }: IAttendance)
         : Promise<{ success: boolean, message: string }> {
-        await this.AttendanceModel.findOneAndUpdate(
-            { qr },
-            { $inc: { absences: 1 } },
-            { new: true, upsert: true }
-        )
+        try {
+            await this.AttendanceModel.findOneAndUpdate(
+                { qr },
+                { $inc: { absences: 1 } },
+                { new: true, upsert: true }
+            )
 
-        return { success: true, message: 'Absent created sucessfully!' }
+            return { success: true, message: 'Absent created sucessfully!' }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Qr user data failed to insert absent.', error }, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 
     async ResetAttendance()
         : Promise<{ success: boolean, message: string }> {
-        await this.AttendanceModel.deleteMany({})
+        try {
+            await this.AttendanceModel.deleteMany({})
 
-        return { success: true, message: 'Attendance resetted sucessfully!' }
+            return { success: true, message: 'Attendance resetted sucessfully!' }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Qr user data failed to reset attendance.', error }, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    decodeBase64(encodedData: string): string {
+        return Buffer.from(encodedData, 'base64').toString('utf-8');
+    }
+
+    removeBaseUrl(data: string): string {
+        const baseUrl = `${process.env.SERVER_HOST}`
+        return data.replace(baseUrl, '')
     }
 }
