1b7e8d947321ff5f65793f2542a5683975aec5f3
sdevmarc <suanieeee@yahoo.com>
2024-09-27 04:02:39 +0800

    Refactor main.ts to remove unused imports

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AuthGuard } from './auth/auth.guard';
-import { JwtService } from '@nestjs/jwt';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
