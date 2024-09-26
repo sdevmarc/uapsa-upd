2426053e74b0953604428808469a204f50a4a8b5
sdevmarc <suanieeee@yahoo.com>
2024-09-26 23:46:40 +0800

    Refactor points service to handle QR code data retrieval and manipulation

--- server/src/points/points.service.ts
+++ server/src/points/points.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { IPoints } from './points.interface';
@@ -12,32 +12,55 @@ export class PointsService {
     ) { }
 
     async InsertPoint({ qr }: { qr: string })
-        : Promise<{ success: boolean, message: string }> {
-        const isqr = await this.QrModel.findById(qr)
-        if (!isqr) return { success: false, message: 'Qr is not registered.' }
-        await this.PointModel.findOneAndUpdate(
-            { qr },
-            { $inc: { points: 1 } },
-            { new: true, upsert: true }
-        )
-        return { success: true, message: 'Point added to the user successfully!' }
+        : Promise<{ success: boolean, message: string, qr?: string }> {
+        try {
+            const decoded_base64 = this.decodeBase64(qr)
+
+            const isqr = await this.QrModel.findById(decoded_base64)
+            if (!isqr) return { success: false, message: 'Qr is not registered.' }
+            await this.PointModel.findOneAndUpdate(
+                { qr: decoded_base64 },
+                { $inc: { points: 1 } },
+                { new: true, upsert: true }
+            )
+            return { success: true, message: 'Point added to the user successfully!', qr: isqr._id.toString() }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Qr user data failed to insert.', error }, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 
     async LessPoint({ qr }: { qr: string })
         : Promise<{ success: boolean, message: string }> {
-        const isqr = await this.QrModel.findById(qr)
-        if (!isqr) return { success: false, message: 'Qr is not registered.' }
-        await this.PointModel.findOneAndUpdate(
-            { qr },
-            { $inc: { points: -1 } },
-            { new: true, upsert: true }
-        )
-        return { success: true, message: 'Point resetted successfully!' }
+        try {
+            const isqr = await this.QrModel.findById(qr)
+            if (!isqr) return { success: false, message: 'Qr is not registered.' }
+            await this.PointModel.findOneAndUpdate(
+                { qr },
+                { $inc: { points: -1 } },
+                { new: true, upsert: true }
+            )
+            return { success: true, message: 'Point resetted successfully!' }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Qr user data failed to less point.', error }, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 
     async ResetPoints()
         : Promise<{ success: boolean, message: string }> {
-        await this.PointModel.deleteMany({})
-        return { success: true, message: 'Points resetted successfully!' }
+        try {
+            await this.PointModel.deleteMany({})
+            return { success: true, message: 'Points resetted successfully!' }
+        } catch (error) {
+            throw new HttpException({ success: false, message: 'Qr user data failed to reset points.', error }, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    decodeBase64(encodedData: string): string {
+        return Buffer.from(encodedData, 'base64').toString('utf-8');
+    }
+
+    removeBaseUrl(data: string): string {
+        const baseUrl = `${process.env.SERVER_HOST}`
+        return data.replace(baseUrl, '')
     }
 }
