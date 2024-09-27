adcc8713563662d3eb06abbb82432939850193f6
sdevmarc <suanieeee@yahoo.com>
2024-09-27 18:59:47 +0800

    Refactor system module to update system UI schema

--- server/src/system/system.schema.ts
+++ server/src/system/system.schema.ts
@@ -1,12 +1,33 @@
 import mongoose from "mongoose";
 
-export const SystemHeaderUISchema = new mongoose.Schema({
-    public_id: String,
-    headerTitle: String,
-    headerIconUrl: String
-}, { timestamps: true })
-
-export const SystemSignInUISchema = new mongoose.Schema({
-    public_id: String,
-    bgImageUrl: String
+export const SystemUISchema = new mongoose.Schema({
+    index: {
+        type: Number,
+        required: true,
+        default: 1
+    },
+    header: {
+        public_id: {
+            type: String,
+            default: ''
+        },
+        headerTitle: {
+            type: String,
+            default: ''
+        },
+        headerIconUrl: {
+            type: String,
+            default: ''
+        }
+    },
+    signin: {
+        public_id: {
+            type: String,
+            default: ''
+        },
+        bgImageUrl: {
+            type: String,
+            default: ''
+        },
+    }
 }, { timestamps: true })
