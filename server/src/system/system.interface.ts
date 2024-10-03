ff58da4612d9ab5e642cdef33a79044d26238e34
Marc Edison <suanieeee@yahoo.com>
2024-10-03 13:00:41 +0800

    Refactor system UI interface to include background image and header icon URLs

--- server/src/system/system.interface.ts
+++ server/src/system/system.interface.ts
@@ -11,11 +11,19 @@ interface ISystemSignIn {
 export interface ISystemUI {
     index?: number
     header_title?: string
+
     header_icon?: ISystemHeader
     sign_in?: ISystemSignIn
+
+    bg_image_public_id: string
+    bg_image_url: string
+
+    header_icon_public_id: string
+    header_icon_url: string
 }
 
 export interface IPromiseSystemUI {
     success: boolean
     message: string
+    data?: ISystemUI
 }
