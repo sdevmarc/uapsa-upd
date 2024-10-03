8b0c84e208e18979d1dffc7f23d770953c5fd3fc
Marc Edison <suanieeee@yahoo.com>
2024-10-03 13:00:10 +0800

    Refactor main.tsx to update header title, handle header icon, and sign-in background

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,55 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Routes from './routes'
 import { ThemeProvider } from './hooks/useTheme'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
 import { Toaster } from "@/components/ui/sonner"
+import { API_FIND_SYSTEM_UI } from './api'
 
 const queryClient = new QueryClient()
+
+const App = () => {
+    const { data: systemui, isFetched: systemuiFetched } = useQuery({
+        queryFn: () => API_FIND_SYSTEM_UI(),
+        queryKey: ['systemui']
+    })
+
+    useEffect(() => {
+        const changeFavicon = (url: string) => {
+            const link = document.querySelector("link[rel~='icon']")
+            if (!link) {
+                const newLink = document.createElement('link')
+                newLink.rel = 'icon'
+                newLink.href = url
+                document.getElementsByTagName('head')[0].appendChild(newLink)
+            } else {
+                (link as HTMLLinkElement).href = url
+            }
+        }
+        if (systemuiFetched && systemui) {
+            const logo = systemui.data.header_icon.header_icon_url
+            const title = systemui.data.header_title;
+            changeFavicon(logo)
+            document.title = title || 'UAPSA-UPD';
+        }
+
+    }, [systemuiFetched, systemui]);
+
+    return (
+        <>
+            <RouterProvider router={Routes} />
+            <Toaster />
+        </>
+    );
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <QueryClientProvider client={queryClient}>
-                <RouterProvider router={Routes} />
-                <Toaster />
+                <App />
             </QueryClientProvider>
         </ThemeProvider>
     </React.StrictMode>,
