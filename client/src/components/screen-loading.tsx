b24fd9633b5692dc9c8908937973f19340140440
Marc Edison <suanieeee@yahoo.com>
2024-10-03 13:00:17 +0800

    Refactor screen-loading component to update loading UI

--- client/src/components/screen-loading.tsx
+++ client/src/components/screen-loading.tsx
@@ -1,6 +1,6 @@
 import { waveform } from 'ldrs'
-import { getRandomLoadingMessage } from './loading-messages'
 import { useState, useEffect } from 'react';
+import Cycling from '@/assets/cycling-man.svg'
 
 waveform.register()
 
@@ -28,22 +28,19 @@ export default function ScreenLoading() {
     }, []);
 
     return (
-        <main className='z-[10] fixed top-0 left-0 w-full h-screen bg-background flex flex-col justify-center items-center gap-8 px-4'>
+        <main className='z-[10] fixed top-0 left-0 w-full h-screen bg-background flex flex-col justify-center items-center gap-3 px-4'>
+            <div className="w-[10rem] h-[10rem] overflow-hidden">
+                <img src={Cycling} alt="Loading image" className='w-full h-full object-cover' />
+            </div>
             <l-waveform
                 size="35"
                 stroke="3.5"
                 speed="1"
                 color={`${htmlClass === 'dark' ? 'white' : 'black'}`}
             ></l-waveform>
-            <div className="flex flex-col gap-2">
-                <h1 className='font-medium text-[1rem] text-primary text-center'>
-                    Loading...
-                </h1>
-                <h1 className='font-semibold text-[1.2rem] text-primary text-center'>
-                    {getRandomLoadingMessage()}
-                </h1>
-            </div>
-
+            <h1 className='font-medium text-[1rem] text-primary text-center'>
+                Hold on... I'm loading
+            </h1>
         </main>
     )
 }
