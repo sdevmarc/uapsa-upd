c2a71003b759bf0a0a108f06253f92f4ba8de3bb
sdevmarc <suanieeee@yahoo.com>
2024-10-04 03:32:20 +0800

    Refactor Home page to dynamically update header title based on system UI settings

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from 'react-router-dom'
 import BlurImage from '@/assets/BlurBlob.png'
 import ScreenLoading from '@/components/screen-loading';
 import '@fontsource/fredoka-one';
+import { API_FIND_SYSTEM_UI } from '@/api';
+import { useQuery } from '@tanstack/react-query';
 
 export default function Home() {
     const [imageloading, setImageLoading] = useState<boolean>(true)
@@ -13,17 +15,25 @@ export default function Home() {
         if (token) return navigate('/dashboard')
     }, [token, navigate]);
 
+    const { data: systemui, isLoading: systemuiLoading, isFetched: systemuiFetched } = useQuery({
+        queryFn: () => API_FIND_SYSTEM_UI(),
+        queryKey: ['systemui']
+    })
+
     return (
         <>
             <div className="relative overflow-hidden w-full h-screen flex flex-col justify-start items-center">
-                {imageloading && <ScreenLoading />}
+                {(imageloading || systemuiLoading) && <ScreenLoading />}
                 <img src={BlurImage} alt="Blur Image Asset" className="absolute top-[-5rem] left-[-10rem] w-[20rem] h-[20rem] scale-[4]" loading='lazy' onLoad={() => setImageLoading(false)} onError={() => setImageLoading(false)} />
                 <img src={BlurImage} alt="Blur Image Asset" className="absolute top-[-5rem] right-[-10rem] w-[20rem] h-[20rem] scale-[4]" loading='lazy' onLoad={() => setImageLoading(false)} onError={() => setImageLoading(false)} />
                 <img src={BlurImage} alt="Blur Image Asset" className="absolute top-[50rem] left-[30rem] w-[20rem] h-[20rem] scale-[4]" loading='lazy' onLoad={() => setImageLoading(false)} onError={() => setImageLoading(false)} />
                 <div className="z-[1] w-full h-full max-w-[90rem] px-4">
                     <main className="w-full h-full flex flex-col justify-center items-center gap-4">
                         <h1 className='text-center text-[4rem] sm:text-[4rem] md:text-[4rem] lg:text-[5rem] xl:text-[5rem] font-semibold text-primary font-fredoka'>
-                            PON-HUB
+                            {
+                                systemuiFetched &&
+                                systemui?.data?.header_title || 'PON-HUB'
+                            }
                         </h1>
                         <p className='text-sm text-primary text-center'>
                             Track attendance and points effortlessly with a click. Simple, smart, and always on top!
