650b3e20a06f190d049c00fefd1ed309ba8d71d9
sdevmarc <suanieeee@yahoo.com>
2024-10-04 21:06:39 +0800

    Refactor routes.tsx to remove unused imports and reorder page imports

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,14 +1,13 @@
 import { createBrowserRouter } from "react-router-dom"
-import ScanPoints from "@/pages/scan-points"
 import SignIn from "@/pages/sign-in"
 import SignUp from "@/pages/sign-up"
 import ScanAttendance from "@/pages/scan-attendance"
 import NotFoundPage from "@/pages/error-not-found"
 import ViewPointsAttendance from "@/pages/view-points-attendance"
-import Home from "@/pages/Home"
-import Dashboard from "./pages/Dashboard"
-import Management from "./pages/Management"
 import Settings from "./pages/settings"
+import Dashboard from "./pages/dashboard"
+import Home from "./pages/home"
+import Management from "./pages/management"
 
 const Routes = createBrowserRouter([
     { path: '/', element: <Home /> },
@@ -16,7 +15,6 @@ const Routes = createBrowserRouter([
     { path: '/signup', element: <SignUp /> },
     { path: '/dashboard', element: <Dashboard /> },
     { path: '/scanattendance', element: <ScanAttendance /> },
-    { path: '/scanpoints', element: <ScanPoints /> },
     { path: '/management', element: <Management /> },
     { path: '/viewstatus/:qr', element: <ViewPointsAttendance /> },
     { path: '/settings', element: <Settings /> },
