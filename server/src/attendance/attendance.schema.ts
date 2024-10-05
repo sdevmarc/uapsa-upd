b68bf53f75373b9835282cce127754e1f15a37d2
sdevmarc <suanieeee@yahoo.com>
2024-10-05 23:48:06 +0800

    Refactor attendance schema to comment out unused absences property

--- server/src/attendance/attendance.schema.ts
+++ server/src/attendance/attendance.schema.ts
@@ -11,9 +11,9 @@ export const AttendanceSchema = new mongoose.Schema({
         required: true,
         default: 0
     },
-    absences: {
-        type: Number,
-        required: true,
-        default: 0
-    }
+    // absences: {
+    //     type: Number,
+    //     required: true,
+    //     default: 0
+    // }
 }, { timestamps: true })
