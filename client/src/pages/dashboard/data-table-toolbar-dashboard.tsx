7c268a5d7e3ce3a195299103d5bcfd08e8e5dcdc
sdevmarc <suanieeee@yahoo.com>
2024-10-11 21:28:47 +0800

    Refactor DataTableToolbarDashboard component to add functionality for less points and less attendance

--- client/src/pages/dashboard/data-table-toolbar-dashboard.tsx
+++ client/src/pages/dashboard/data-table-toolbar-dashboard.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Cross2Icon } from "@radix-ui/react-icons"
+import { Cross2Icon, DotsHorizontalIcon } from "@radix-ui/react-icons"
 import { Table } from "@tanstack/react-table"
 
 import QRCode from 'qrcode'
@@ -10,13 +10,14 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import React, { useEffect, useRef, useState } from "react"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { API_CREATE_POINT, API_CREATE_QR, API_DELETE_MULTIPLE_QR, API_RESET_QR_PROGRESS } from "@/api"
+import { API_CREATE_POINT, API_CREATE_QR, API_DELETE_MULTIPLE_QR, API_LESS_MULTIPLE_ATTENDANCE, API_LESS_MULTIPLE_POINTS, API_RESET_QR_PROGRESS } from "@/api"
 import { useNavigate } from "react-router-dom"
 import { toast } from "sonner"
 import ScreenLoading from "@/components/screen-loading"
 import { DialogContainer } from "@/components/dialog"
 import { AlertDialogConfirmation } from "@/components/alert-dialog"
 import { IQRSchema } from "@/components/data-table-components/schema"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
 interface DataTableToolbarProps<TData> {
     table: Table<TData>
@@ -54,6 +55,10 @@ export function DataTableToolbarDashboard<TData>({
         name: '',
     })
     const [idNumbers, setIdNumbers] = useState<string[]>([])
+    const [less, setLess] = useState({
+        points: 0,
+        attended: 0
+    })
     const [point, setPoint] = useState<number>(0)
     const token = localStorage.getItem('token') || ''
 
@@ -142,6 +147,26 @@ export function DataTableToolbarDashboard<TData>({
         }
     })
 
+    const { mutateAsync: lesspoints, isPending: lesspointsLoading } = useMutation({
+        mutationFn: API_LESS_MULTIPLE_POINTS,
+        onSuccess: async (data) => {
+            if (!data.success) return toast("Uh, oh! Something went wrong.", { description: 'Failed to reset users.' })
+            await queryClient.invalidateQueries({ queryKey: ['qrstatus'] })
+            await queryClient.refetchQueries({ queryKey: ['qrstatus'] })
+            return toast("Yay, Success!", { description: data.message })
+        }
+    })
+
+    const { mutateAsync: lessattendance, isPending: lessattendanceLoading } = useMutation({
+        mutationFn: API_LESS_MULTIPLE_ATTENDANCE,
+        onSuccess: async (data) => {
+            if (!data.success) return toast("Uh, oh! Something went wrong.", { description: 'Failed to reset users.' })
+            await queryClient.invalidateQueries({ queryKey: ['qrstatus'] })
+            await queryClient.refetchQueries({ queryKey: ['qrstatus'] })
+            return toast("Yay, Success!", { description: data.message })
+        }
+    })
+
     const handleAddPointChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { value } = e.target
         setPoint(Number(value))
@@ -167,12 +192,35 @@ export function DataTableToolbarDashboard<TData>({
         onResetSelections()
     }
 
+    const handleLessPoints = async () => {
+        if (isNaN(Number(less.points))) {
+            toast("Uh, oh! Something went wrong.", { description: "Point should be a number." })
+            return
+        }
+        if (isNaN(Number(less.points === 0))) return
+
+        await lesspoints({ idNumber: idNumbers, points: less.points, token })
+        onResetSelections()
+    }
+
+
+    const handleLessAttendance = async () => {
+        if (isNaN(Number(less.attended))) {
+            toast("Uh, oh! Something went wrong.", { description: "Point should be a number." })
+            return
+        }
+        if (isNaN(Number(less.attended === 0))) return
+
+        await lessattendance({ idNumber: idNumbers, attended: less.attended, token })
+        onResetSelections()
+    }
+
     const isLoading = qrLoading || insertpointLoading || deleteqruserLoading || resetprogressLoading
 
     return (
         <div className="flex flex-wrap items-center justify-between" >
             {isLoading && <ScreenLoading />}
-            < canvas ref={canvasRef} style={{ display: 'none' }} />
+            <canvas ref={canvasRef} style={{ display: 'none' }} />
             < div className="flex flex-1 flex-wrap items-center gap-2" >
                 <Input
                     placeholder="Search ID Number..."
@@ -183,27 +231,90 @@ export function DataTableToolbarDashboard<TData>({
                     className="h-8 w-[20rem] lg:w-[25rem] placeholder:text-muted"
                 />
                 {
+                    isFiltered && (
+                        <Button
+                            variant="ghost"
+                            onClick={() => table.resetColumnFilters()}
+                            className="h-8 px-2 lg:px-3"
+                        >
+                            Reset
+                            <Cross2Icon className="ml-2 h-4 w-4" />
+                        </Button>
+                    )
+                }
+                {
                     isrows &&
                     <>
-                        <DialogContainer
-                            disabled={insertpointLoading}
-                            submit={handleAddPoint}
-                            title="Add Point"
-                            description="Please fill-out the required fields."
-                            Trigger={
-                                <Button variant={`outline`} size={`sm`}>
-                                    Add Point
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button
+                                    variant="ghost"
+                                    className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+                                >
+                                    <DotsHorizontalIcon className="h-4 w-4" />
+                                    <span className="sr-only">Open menu</span>
                                 </Button>
-                            }
-                            children={
-                                <>
-                                    <Label htmlFor="points" className="text-right">
-                                        Please input a valid number.
-                                    </Label>
-                                    <Input disabled={insertpointLoading} required id="points" name="points" onChange={handleAddPointChange} placeholder="eg. 2" className="col-span-3 placeholder:text-muted" />
-                                </>
-                            }
-                        />
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end" className="w-[160px] flex flex-col gap-1">
+                                <DialogContainer
+                                    disabled={insertpointLoading}
+                                    submit={handleAddPoint}
+                                    title="Add Point"
+                                    description="Please fill-out the required fields."
+                                    Trigger={
+                                        <Button variant={`outline`} size={`sm`}>
+                                            Add Point
+                                        </Button>
+                                    }
+                                    children={
+                                        <>
+                                            <Label htmlFor="points" className="text-right">
+                                                Please input a valid number.
+                                            </Label>
+                                            <Input disabled={insertpointLoading} required id="points" name="points" onChange={handleAddPointChange} placeholder="eg. 2" className="col-span-3 placeholder:text-muted" />
+                                        </>
+                                    }
+                                />
+                                <DialogContainer
+                                    disabled={insertpointLoading}
+                                    submit={handleLessPoints}
+                                    title="Less Point"
+                                    description="Please fill-out the required fields."
+                                    Trigger={
+                                        <Button variant={`outline`} size={`sm`}>
+                                            Less Point
+                                        </Button>
+                                    }
+                                    children={
+                                        <>
+                                            <Label htmlFor="points" className="text-right">
+                                                Please input a valid number.
+                                            </Label>
+                                            <Input disabled={lesspointsLoading} required id="points" name="points" onChange={(e) => setLess(prev => ({ ...prev, points: Number(e.target.value) }))} placeholder="eg. 20" className="col-span-3 placeholder:text-muted" />
+                                        </>
+                                    }
+                                />
+                                <DialogContainer
+                                    disabled={insertpointLoading}
+                                    submit={handleLessAttendance}
+                                    title="Less Attendance"
+                                    description="Please fill-out the required fields."
+                                    Trigger={
+                                        <Button variant={`outline`} size={`sm`}>
+                                            Less Attendance
+                                        </Button>
+                                    }
+                                    children={
+                                        <>
+                                            <Label htmlFor="attendance" className="text-right">
+                                                Please input a valid number.
+                                            </Label>
+                                            <Input disabled={lessattendanceLoading} required id="attendance" name="attendance" onChange={(e) => setLess(prev => ({ ...prev, attended: Number(e.target.value) }))} placeholder="eg. 20" className="col-span-3 placeholder:text-muted" />
+                                        </>
+                                    }
+                                />
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                         <AlertDialogConfirmation
                             btnTitle="Reset Progress"
                             title="Are you sure?"
@@ -217,18 +328,6 @@ export function DataTableToolbarDashboard<TData>({
                             btnContinue={handleDeleteQrUser}
                         />
                     </>
-                }
-                {
-                    isFiltered && (
-                        <Button
-                            variant="ghost"
-                            onClick={() => table.resetColumnFilters()}
-                            className="h-8 px-2 lg:px-3"
-                        >
-                            Reset
-                            <Cross2Icon className="ml-2 h-4 w-4" />
-                        </Button>
-                    )
                 }
             </div >
             <div className="flex gap-2">
